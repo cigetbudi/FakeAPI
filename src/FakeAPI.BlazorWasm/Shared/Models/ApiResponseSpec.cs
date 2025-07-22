da62fc1222d8e196b4a1f9cc9724fd70ca886c8d
sigitraya <sigit.darmawan@bankraya.co.id>
2025-07-22 07:17:31 +0700

    Add more components

---
+++ src/FakeAPI.BlazorWasm/Shared/Models/ApiResponseSpec.cs
@@ -0,0 +1,8 @@
+namespace FakeAPI.BlazorWasm.Shared.Models;
+
+public class ApiResponseSpec
+{
+    public string Name { get; set; } = string.Empty;
+    public string DataType { get; set; } = string.Empty;
+    public string Notes { get; set; } = string.Empty;
+}
